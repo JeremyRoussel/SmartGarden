b5ba0a8455659c381e7e23f3ca8eb0e5ba3fec20
cortezmb <michael.cortez78@gmail.com>
2020-07-31 11:56:47 -0500

    encrypted password for register

--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs'); //Require bcrypt
+const db = require('../models'); //Require db from models directory
+
+
 
 // body-parser
 
 // database link to express
 
-
+//render to register.ejs
 router.get('/register', (req, res) => {
 
     res.render('register')
+})
+
+//Capture username, password and email from registration.ejs. 
+router.post('/register', (req, res) => {
+
+    let password = req.body.password;
+    let email = req.body.email;
+
+    let passwordEncrypted = bcrypt.hashSync(password, 8); //encrypt password
+
+    db.users.create({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        pwHex: passwordEncrypted, 
+    })
+    .then(user => {
+        res.redirect('/login'); //if password matches will redirect to login page
+    })
+    .catch(error => {
+        res.redirect('/404'); //will redirect to 404 error page
+    })
 })
 
 
