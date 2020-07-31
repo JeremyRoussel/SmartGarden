8fa8ac8ff06c5f78e9fb886ddca649b602974c27
Woody Connell <woodyconnell@gmail.com>
2020-07-31 13:56:06 -0500

    login form almost working

--- app.js
+++ app.js
@@ -12,24 +12,25 @@ app.use(express.static('public'));
 // link to views folder
 app.set('view engine', 'ejs');
 
+
 // create routes
-app.use(require('./routes/index')) // home page, unprotected
+app.use(require('./routes/index')); // home page, unprotected
 
-app.use(require('./routes/login')) // login page
+app.use(require('./routes/login')); // login page
 
-app.use(require('./routes/register')) // registration page
+app.use(require('./routes/register')); // registration page
 
-app.use(require('./routes/user'))  // user homepage and plants
+app.use(require('./routes/user'));  // user homepage and plants
 
-app.use(require('./routes/new'))  // create plant for a user
+app.use(require('./routes/new'));  // create plant for a user
 
-app.use(require('./routes/plant')) // view data for a plant
+app.use(require('./routes/plant')); // view data for a plant
 
-app.use(require('./routes/api')) // post data for a plant to database, lookup data for plant
+app.use(require('./routes/api')); // post data for a plant to database, lookup data for plant
 
-app.use(require('./routes/404')) // error page
+app.use(require('./routes/404')); // error page
 
-app.use(require('./routes/aboutus')) // about us
+app.use(require('./routes/aboutus')); // about us
 
 
 // Start Server
--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models');
 const bcrypt = require('bcryptjs');
+const db = require('../models'); 
+const bodyParser = require('body-parser');
+
+// body-parser
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login');
 })
 
+// database link to express
 router.post('/login', async (req, res) => {
 
   try {
-      let username = req.body.username; //form
-      let password = req.body.password; //form
+      let email = req.body.email; // form
+      let password = req.body.password; // form
 
-      console.log(username, password);
-
-      let results = await db.users.findAll({ where: { username: username } });
-      //results is an array of objects from database 
+      let results = await db.users.findAll({ where: {email: email}});
+      // results is an array of objects from database 
 
       if (results.length > 0) {
-          //test password 
+          // test password 
 
           bcrypt.compare(password, results[0].password, (err, response) => {
-              //there is a match in passwords
+              // there is a match in passwords
               if (response) {
-                  req.session.username = username;  //username is an object on the session object
+                  req.session.email = email;  // username is an object on the session object
                   req.session.role = 1;
                   res.redirect('/')
               }
               else {
-                  //no match found for passwords
+                  // no match found for passwords
                   res.redirect('/error');
               }
           })
@@ -39,9 +42,7 @@ router.post('/login', async (req, res) => {
   catch {
       res.status(211).redirect('/error')
   }
-// body-parser
 
-// database link to express
-
+})
 
 module.exports = router;
