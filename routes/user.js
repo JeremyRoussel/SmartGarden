9f4c877b9bd67a76b71eb2af66322358b9b4e5d5
woody-connell <woodyconnell@gmail.com>
2020-08-04 15:47:45 -0500

    plants are showing up on user view

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models'); // Require db from models directory
+
 
 // body-parser
+// let urlencodedParser = bodyParser.urlencoded({extended: false});
+
 
 // database link to express
-
 
 router.get('/user(/)?(:id)?', (req, res) => {
 
     let user = req.params.id  // Identify the parameter from the URI 
 
-
-    res.render('user', {   // Send the required data to the render
-        user: user,
+    db.plants.findAll()
+    .then(results => {
+        res.render('user', {
+            plants: results,
+        })
     })
+    .catch((error => {
+        res.redirect('/404'); 
+    }))
 })
 
 
